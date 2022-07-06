49e76083866df0b2ff24382404ead696d0867dc3
Cory <cjbenicak@gmail.com>
2022-07-06 10:24:26 -0400

    made vars for DOM elements

--- BlackJack.js
+++ BlackJack.js
@@ -5,6 +5,21 @@ let dealerSum = 0
 let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k', 'a']
 let suits = ['s', 'h', 'c', 'd']
 let deck = []
+const mes = document.querySelector('.message')
+const start = document.querySelector('.start')
+const stay = document.querySelector('.stay')
+const hit = document.querySelector('.hit')
+const reset = document.querySelector('.reset')
+const d1 = document.querySelector('.card.d1')
+const d2 = document.querySelector('.card.d2')
+const d3 = document.querySelector('.card.d3')
+const d4 = document.querySelector('.card.d4')
+const d5 = document.querySelector('.card.d5')
+const p1 = document.querySelector('.card.p1')
+const p2 = document.querySelector('.card.p2')
+const p3 = document.querySelector('.card.p3')
+const p4 = document.querySelector('.card.p4')
+const p5 = document.querySelector('.card.p5')
 
 //FUNCTIONS
 
@@ -31,3 +46,9 @@ const randomCard = () => {
 
 buildDeck()
 shuffleDeck()
+
+// Conditionals
+
+if (playerSum > 21) {
+  mes.innerHTML = 'You went over 21, you lose.'
+}
