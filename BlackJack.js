0913588555e706be5e6cd5ffa094cfb64856a10a
Cory <cjbenicak@gmail.com>
2022-07-06 09:40:23 -0400

    added global variables

--- BlackJack.js
+++ BlackJack.js
@@ -1,2 +1,17 @@
+// GLOBAL VARIABLES
+
 let playerSum = 0
 let dealerSum = 0
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k', 'a']
+let suits = ['s', 'h', 'c', 'd']
+let deck = []
+
+//FUNCTIONS
+
+const buildDeck = () => {
+  for (i = 0; i < values.length; i++) {
+    for (x = 0; x < suits.length; x++) {}
+  }
+}
+
+const shuffleDeck = () => {}
